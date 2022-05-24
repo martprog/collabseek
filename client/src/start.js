7247fe67bf2ff6166d68b11f6a371fe8101f083f
Martín Sotelo <martinscaraguel@gmail.com>
2022-05-24 11:36:39 +0200

    first commit

--- client/src/start.js
+++ client/src/start.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 ReactDOM.render(<HelloWorld />, document.querySelector("main"));
 
 function HelloWorld() {
-    return <div>Hello, World!</div>;
+    return <div>Last project!</div>;
 }
